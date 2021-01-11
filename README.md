f573be722eb4f03caf23c29ba5f9b854a181572b
joji7752 <joji7752@gmail.com>
2021-01-12 01:13:24 +0900

    2.21

--- README.md
+++ README.md
@@ -9,8 +9,8 @@ Cloning Youtube with Vanilla and NodeJS
 - [x] Login
 - [x] Search
 - [ ] User Detail
-- [ ] Edit profile
-- [ ] Change Password
-- [ ] Upload
+- [x] Edit Profile
+- [x] Change Password
+- [x] Upload
 - [ ] Video Detail
 - [ ] Edit Video
---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+     {
+          id: 324393,
+          title: "Video awesome",
+          description: "This is example",
+          view: 24,
+          videoFile: "https://archive.org/details/BigBuckBunny_124",
+          creator: {
+               id: 1212,
+               name:"yeongji",
+               email:"asdfs@mssd.com"
+          }
+     },
+     {
+          id: 1212,
+          title: "Video 2",
+          description: "This is example",
+          view: 24,
+          videoFile: "https://archive.org/details/BigBuckBunny_124",
+          creator: {
+               id: 1212,
+               name:"yeongji",
+               email:"asdfs@mssd.com"
+          }
+     },
+     {
+          id: 32333,
+          title: "Video 3",
+          description: "This is example",
+          view: 24,
+          videoFile: "https://archive.org/details/BigBuckBunny_124",
+          creator: {
+               id: 1212,
+               name:"yeongji",
+               email:"asdfs@mssd.com"
+          }
+     },
+     {
+          id: 5555,
+          title: "Video 4",
+          description: "This is example",
+          view: 24,
+          videoFile: "https://archive.org/details/BigBuckBunny_124",
+          creator: {
+               id: 1212,
+               name:"yeongji",
+               email:"asdfs@mssd.com"
+          }
+     }
+];
